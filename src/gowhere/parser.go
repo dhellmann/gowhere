569207ea8d77e6a2c4c168f72f99b772c7335800
Doug Hellmann <doug@doughellmann.com>
2018-12-28 18:05:23 -0500

    handle comments and blank lines

--- src/gowhere/parser.go
+++ src/gowhere/parser.go
@@ -9,16 +9,23 @@ import (
 
 func ParseRules(fd io.Reader) (*RuleSet, error) {
 	var rules RuleSet
-	line_num := 1
+	line_num := 0
 	input := bufio.NewScanner(fd)
 	for input.Scan() {
-		params := strings.Split(input.Text(), " ")
+		line := strings.Trim(input.Text(), " \t\n")
+		line_num++
+		if len(line) == 0 {
+			continue
+		}
+		if line[0] == '#' {
+			continue
+		}
+		params := strings.Fields(line)
 		err := rules.Add(line_num, params)
 		if err != nil {
 			return nil, fmt.Errorf("Could not parse rule on line %d (%s): %s", 
 				line_num, input.Text(), err)
 		}
-		line_num++
 	}
 	return &rules, nil
 }
